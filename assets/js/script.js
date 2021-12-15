eefe659123cace3ed39c8f8054359321a1df8d64
Michael Hranek <mhranek@comcast.net>
2021-12-15 18:24:07 -0500

    Added current day display and hour rows

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Element references
+var currentDayEl = $('#currentDay');
+
+// Current day in header
+var currentDay = moment().format('dddd, MMMM Do');
+currentDayEl.text(currentDay);
+
+// TODO Color code timeblocks
+
+// TODO Timeblocks are text fields
+
+// TODO Timeblock text is saveable to local storage
