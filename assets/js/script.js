f7da457ec62bdb41a8d558fd97fcc494d0509a1f
Michael Hranek <mhranek@comcast.net>
2021-12-16 01:14:44 -0500

    Added localstorage saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var containerEl = $('.container');
 var currentDay = moment().format('dddd, MMMM Do');
 currentDayEl.text(currentDay);
 
-// TODO Color code timeblocks
+// Color code timeblocks
 // loop through each text field, assigning it to the correct class
 for (var i = 0; i < containerEl.children().length; i++) {
     // Current time of day in hours
@@ -15,19 +15,45 @@ for (var i = 0; i < containerEl.children().length; i++) {
     var rowTime = containerEl.children().eq(i).children().eq(0).children().eq(0).text();
     // convert to number from string
     rowTime = parseFloat(rowTime);
-    if (rowTime < 5) {
-        rowTime = rowTime + 12;
-    }
-    // If time is in the past, give textarea the .past class (default)
-    // If time is in the present, give textarea the .present class
+    if (rowTime < 5) {rowTime = rowTime + 12;}
     if (currentTime > rowTime) {
+        // If time is in the past, give textarea the .past class
         containerEl.children().eq(i).children().eq(1).addClass('past');
     } else if (currentTime == rowTime) {
+        // If time is in the present, give textarea the .present class
         containerEl.children().eq(i).children().eq(1).addClass('present');
     } else {
+        // If time is in the future, give textarea the .future class
         containerEl.children().eq(i).children().eq(1).addClass('future');
-    }
-    // If time is in the future, give textarea the .future class
+    } 
 }
-
-// TODO Timeblock text is saveable to local storage
+// Timeblock text is saveable to local storage
+// Listen for click event on save button
+containerEl.on('click', '.saveBtn', function (event) {
+    var element = $(event.target);
+    // check if target is save icon or save button
+    if (element.is('.fa-save')) {
+        element = element.parent();
+    }
+    storeData(element);
+})
+// Function to store data to localstorage
+function storeData(element) {
+    // key
+    var key = element.parent().children().eq(0).text().trim();
+    // value
+    var value = element.parent().children().eq(1).val();
+    // key value pair put into localstorage
+    localStorage.setItem(key, value);
+}
+// Function to pull data from localstorage
+function retrieveData() {
+    for (var i = 0; i < containerEl.children().length; i++) {
+        // look for localstorage to parse
+        var retrievedKey = containerEl.children().eq(i).children().eq(0).text().trim();
+        var retrievedValue = localStorage.getItem(retrievedKey);
+        containerEl.children().eq(i).children().eq(1).val(retrievedValue);
+    }
+}
+// On page load retrieve data from localstorage
+retrieveData()
